cf345f7076158c4437b373af59e4476809e1104c
Anders Hansson <anders@programlabbet.se>
2017-01-18 22:55:56 +0100

    Always clean out elm-stuff when starting a project to workaround bug with Map.merge! error

--- start.sh
+++ start.sh
@@ -13,5 +13,14 @@ yarn install
 # Start synchronizing the application in the background
 /sync.sh true &
 
+# Clean elm-stuff because there exists a bug that prevents projects to build
+# if the elm-stuff has been corrupted by package definition changes and/or
+# other unintuitive changes.
+#
+# Better safe than sorry - we now completely clean the elm-stuff folder from
+# previous build residues. It will take a bit longer but it will work 100% of
+# the time which is actually a much better outcome :-)
+rm -rf /app-sync/elm-stuff
+
 # Start Brunch web server
 yarn start
