af51e8c2486693013654a01f9f52e93a5e01149e
Anders Hansson <anders@programlabbet.se>
2017-02-05 14:16:13 +0100

    Case 270: Adding support for private Elm packages

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ MAINTAINER Anders Hansson <anders@programlabbet.se>
 RUN apt-get update -y && apt-get install -y rsync && apt-get clean -y
 RUN npm install -g yarn
 RUN yarn global add elm@0.18.0
+RUN yarn global add elm-github-install
 RUN mkdir -p /app /app-sync
 VOLUME /app
 VOLUME /app-sync
--- start.sh
+++ start.sh
@@ -4,14 +4,8 @@
 #
 # (C)Copyright Programlabbet AB, 2016. All rights reserved.
 
-# Initial application sync
+# Initial application synchronization
 /sync.sh false
-
-# Install Elm and Node dependencies
-yarn install
-
-# Start synchronizing the application in the background
-/sync.sh true &
 
 # Clean elm-stuff because there exists a bug that prevents projects to build
 # if the elm-stuff has been corrupted by package definition changes and/or
@@ -21,6 +15,12 @@ yarn install
 # previous build residues. It will take a bit longer but it will work 100% of
 # the time which is actually a much better outcome :-)
 rm -rf /app-sync/elm-stuff
+
+# Install Elm and Node dependencies
+yarn install
+
+# Start synchronizing the application in the background
+/sync.sh true &
 
 # Start Brunch web server
 yarn start
